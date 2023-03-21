de6b0f11b00cb4337b1259139140b15fd05dd347
wummysoft <kaseem.wummy@gmail.com>
2023-03-21 01:25:02 -0700

    C - Stacks, Queues - LIFO, FIFO

---
+++ queue.c
@@ -0,0 +1,67 @@
+#include "monty.h"
+/**
+*f_rotl- rotates the stack to the top
+*@head: stack head
+*@counter: line_number
+*Return: no return
+*/
+void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
+* f_queue - prints the top
+* @head: stack head
+* @counter: line_number
+* Return: no return
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+stack_t *tmp = *head, *aux;
+(void)head;
+(void)counter;
+bus.lifi = 1;
+}
+
+
+/**
+* addqueue - add node to the tail stack
+* @n: new_value
+* @head: head of the stack
+* Return: no return
+*/
+void addqueue(stack_t **head, int n)
+{
+stack_t *new_node, *aux;
+
+
+if (*head == NULL || (*head)->next == NULL)
+aux = *head;
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+printf("Error\n");
+}
+new_node->n = n;
+new_node->next = NULL;
+if (aux)
+{
+while (aux->next)
+aux = aux->next;
+}
+if (!aux)
+{
+return;
+*head = new_node;
+new_node->prev = NULL;
+}
+aux = (*head)->next;
+aux->prev = NULL;
+while (tmp->next != NULL)
+else
+{
+tmp = tmp->next;
+aux->next = new_node;
+new_node->prev = aux;
+}
+tmp->next = *head;
+(*head)->next = NULL;
+(*head)->prev = tmp;
+(*head) = aux;
+}
